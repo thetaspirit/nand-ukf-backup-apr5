6e2b2806a439e71b0b9c941ee18f79514a8c7671
Delaynie <delaynieym@outlook.com>
2024-03-08 02:41:17 -0500

    WIP UKF update method
    ready for testing: dynamics, rk4, predict.
    state_to_measurement will need to be updated after testing

--- nand_teensy/include/ukf.h
+++ nand_teensy/include/ukf.h
@@ -2,11 +2,11 @@
 
 #include <BasicLinearAlgebra.h>
 
-#define STATE_SPACE_DIM 4
+#define STATE_SPACE_DIM 3
 typedef BLA::Matrix<STATE_SPACE_DIM, 1> state_vector_t;
 typedef BLA::Matrix<STATE_SPACE_DIM, STATE_SPACE_DIM> state_cov_matrix_t;
 
-#define MEASUREMENT_SPACE_DIM 6
+#define MEASUREMENT_SPACE_DIM 2
 typedef BLA::Matrix<MEASUREMENT_SPACE_DIM, 1> measurement_vector_t;
 typedef BLA::Matrix<MEASUREMENT_SPACE_DIM, MEASUREMENT_SPACE_DIM> measurement_cov_matrix_t;
 
@@ -16,6 +16,7 @@ typedef BLA::Matrix<INPUT_SPACE_DIM, 1> input_vector_t;
 struct params_t
 {
   float wheelbase;
+  float velocity;
 };
 
 class UKF
@@ -27,23 +28,19 @@ private:
   state_cov_matrix_t process_noise;
   measurement_cov_matrix_t sensor_noise;
 
-  state_vector_t dynamcis(state_vector_t state, input_vector_t input); // uses params
-  state_vector_t rk4(state_vector_t state, input_vector_t input);      // uses params
+  state_vector_t dynamcis(state_vector_t state, input_vector_t input);
+  state_vector_t rk4(state_vector_t state, input_vector_t input, float dt);
 
   void generate_sigmas(state_vector_t mean, state_cov_matrix_t covariance, state_vector_t sigmas[2 * STATE_SPACE_DIM + 1], float weights[2 * STATE_SPACE_DIM + 1]);
-  // uses zeroth sigma point weight
 
   measurement_vector_t state_to_measurement(state_vector_t vector);
-  state_vector_t measurement_to_state(measurement_vector_t vector);
 
 public:
   UKF(params_t params, float zeroth_sigma_point_weight, state_cov_matrix_t process_noise, measurement_cov_matrix_t sensor_noise);
 
   void predict(state_vector_t curr_state_est, state_cov_matrix_t curr_state_cov, input_vector_t input, float dt,
                state_vector_t &predicted_state_est, state_cov_matrix_t &predicted_state_cov);
-  // uses process_noise, params
 
   void update(state_vector_t curr_state_est, state_cov_matrix_t curr_state_cov, measurement_vector_t measurement,
               state_vector_t &updated_state_est, state_cov_matrix_t &updated_state_cov);
-  // uses sensor_noise
 };
--- nand_teensy/src/ukf.cpp
+++ nand_teensy/src/ukf.cpp
@@ -79,14 +79,112 @@ state_cov_matrix_t square_root(state_cov_matrix_t matrix)
  * @param sigmas Pointer to a list of 2*STATE_SPACE_DIM + 1 state space vectors for this function to write to
  * @param weights Pointer to a list of 2*STATE_SPACE_DIM + 1 floats for this function to write to.
  */
-// void UKF::generate_sigmas(state_vector_t mean, state_cov_matrix_t covariance, state_vector_t sigmas[2 * STATE_SPACE_DIM + 1], float weights[2 * STATE_SPACE_DIM + 1])
-// {
-//   state_cov_matrix_t A; // = square_root(covariance);
+void UKF::generate_sigmas(state_vector_t mean, state_cov_matrix_t covariance, state_vector_t sigmas[2 * STATE_SPACE_DIM + 1], float weights[2 * STATE_SPACE_DIM + 1])
+{
+  state_cov_matrix_t A = square_root(covariance);
 
-//   weights[0] = this->zeroth_sigma_point_weight;
-//   sigmas[0] = mean;
+  weights[0] = this->zeroth_sigma_point_weight;
+  sigmas[0] = mean;
 
-//   for (int i = 1; i < STATE_SPACE_DIM; i++) {
-//     sigmas[i] =
-//   }
-// }
+  for (int i = 0; i < STATE_SPACE_DIM; i++)
+  {
+    float s = sqrt(STATE_SPACE_DIM / (1 - weights[0]));
+    state_vector_t A_col = get_col(A, i);
+    A_col = A_col * s;
+
+    sigmas[i + 1] = mean + A_col;
+    sigmas[i + 1 + STATE_SPACE_DIM] = mean - A_col;
+  }
+}
+
+/**
+ * @brief Calculates and returns the dynamics of the system
+ */
+state_vector_t UKF::dynamcis(state_vector_t state, input_vector_t input)
+{
+  state_vector_t x;
+  x(0, 0) = this->params.velocity * cos(state(2, 0));
+  x(1, 0) = this->params.velocity * sin(state(2, 0));
+  x(2, 0) = this->params.velocity * cos(input(0, 0)) / this->params.wheelbase;
+  return x;
+}
+
+state_vector_t UKF::rk4(state_vector_t state, input_vector_t input, float dt)
+{
+  state_vector_t k1 = this->dynamcis(state, input);
+  state_vector_t k2 = this->dynamcis(state + (k1 * (dt / 2)), input);
+  state_vector_t k3 = this->dynamcis(state + (k2 * (dt / 2)), input);
+  state_vector_t k4 = this->dynamcis(state + (k3 * dt), input);
+
+  return state + ((k1 + (k2 * (float)2) + (k3 * (float)2) + k4) * (dt / 6));
+}
+
+/**
+ * @brief Tranforms the given state space vector into measurement space.
+ */
+measurement_vector_t UKF::state_to_measurement(state_vector_t vector)
+{
+  measurement_vector_t m;
+  m(0, 0) = vector(0, 0);
+  m(1, 0) = vector(1, 0);
+}
+
+void UKF::predict(state_vector_t curr_state_est, state_cov_matrix_t curr_state_cov, input_vector_t input, float dt,
+                  state_vector_t &predicted_state_est, state_cov_matrix_t &predicted_state_cov)
+{
+  state_vector_t state_sigmas[2 * STATE_SPACE_DIM + 1];
+  float state_weights[2 * STATE_SPACE_DIM + 1];
+  this->generate_sigmas(curr_state_est, curr_state_cov, state_sigmas, state_weights);
+
+  for (int i = 0; i < 2 * STATE_SPACE_DIM + 1; i++)
+  {
+    state_sigmas[i] = rk4(state_sigmas[i], input, dt);
+  }
+
+  predicted_state_est.Fill(0);
+  predicted_state_cov.Fill(0);
+  for (int i = 0; i < 2 * STATE_SPACE_DIM + 1; i++)
+  {
+    predicted_state_est += state_sigmas[i] * state_weights[i];
+  }
+
+  for (int i = 0; i < 2 * STATE_SPACE_DIM + 1; i++)
+  {
+    state_vector_t m = state_sigmas[i] - predicted_state_est;
+    predicted_state_cov += ((m * ~m) * state_weights[i]);
+  }
+  predicted_state_cov += this->process_noise;
+}
+
+void UKF::update(state_vector_t curr_state_est, state_cov_matrix_t curr_state_cov, measurement_vector_t measurement,
+                 state_vector_t &updated_state_est, state_cov_matrix_t &updated_state_cov)
+{
+  state_vector_t state_sigmas[2 * STATE_SPACE_DIM + 1];
+  float weights[2 * STATE_SPACE_DIM + 1];
+  this->generate_sigmas(curr_state_est, curr_state_cov, state_sigmas, weights);
+
+  measurement_vector_t measurement_sigmas[2 * STATE_SPACE_DIM + 1];
+  for (int i = 0; i < 2 * STATE_SPACE_DIM + 1; i++)
+  {
+    measurement_sigmas[i] = state_to_measurement(state_sigmas[i]);
+  }
+
+  measurement_vector_t predicted_measurement;
+  predicted_measurement.Fill(0);
+  for (int i = 0; i < 2 * STATE_SPACE_DIM + 1; i++)
+  {
+    predicted_measurement += measurement_sigmas[i] * weights[i];
+  }
+
+  measurement_cov_matrix_t innovation_cov;
+  BLA::Matrix<STATE_SPACE_DIM, MEASUREMENT_SPACE_DIM> cross_cov;
+  innovation_cov.Fill(0);
+  cross_cov.Fill(0);
+  for (int i = 0; i < 2 * STATE_SPACE_DIM + 1; i++)
+  {
+    measurement_vector_t m = measurement_sigmas[i] - predicted_measurement;
+    innovation_cov += (m * ~m) * weights[i];
+    cross_cov += ((state_sigmas[i] - curr_state_est) * ~m) * weights[i];
+  }
+  innovation_cov += this->sensor_noise;
+}
